4b03fb0f53afb73a443514b55dd62a89eef0607f
Haitao Wang <wanght@vt.edu>
2018-10-23 17:05:28 -0400

    code clean up

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,9 @@
-""" UNIT 1(Maanav & Naman) """
-import json, nltk, string, requests, numpy
+###
+## Feature Extraction for cs4984/5984 Project
+## Team 8 | Oct 2018
+###
+
+import json, nltk, string, requests, numpy, itertools
 import nltk
 import string
 import pprint
@@ -28,7 +32,7 @@ def get_synsets(keyword):
     syns = set()
     for syn in wordnet.synsets(keyword):
         for lemma in syn.lemmas():
-            syns.add(lemma.name())
+            syns.add(lemma.name().lower())
     return list(syns)
 
 
@@ -86,8 +90,10 @@ def count_frequency_keyword_only(keywords, document):
 
 
 """ CONFIG """
-NoDAPL_file = "NoDAPL-top50.json"
-stopwords_file = "stopwords_new.txt"
+NoDAPL_file = "data\NoDAPL-top50.json"
+stopwords_file = "data\stopwords_new.txt"
+stopwords_list = ["'s", '...', '-800', 'mr.', 'de', 'een', '-0800', 'www.senate.gov', "''", "``", "\\n", "'s", "'re", "the"]
+wikipage_link = 'https://en.wikipedia.org/wiki/NODAPL' # 'https://en.wikipedia.org/wiki/Dakota_Access_Pipeline'
 
 """ LOAD DATA """
 test_document = "This movement tribe is not just about a pipeline.  You folk are not fighting for a reroute, or a better process in the white man courts.  We are fighting for our rights as the indigenous peoples of this land; we are fighting for our liberation, and the liberation of Unci Maka, Mother Earth.  We want every last oil and gas pipe removed from her body.  We want healing.  We want clean water.  We want to determine our own future. Each one of us is fighting for our grandchildren, and their grandchildren, and for our relatives who cannot speak or fight back. Imagine if we had stood together on October 27th, the day they pushed us out the Treaty Camp we built in the very path of the Black Snake -- our most powerful position in this entire struggle. What if our own people had not negotiated away our power? What if our people had not opened the roads and then turned to march against us with outstretched arms, in line with the riot police and armored vehicles?  Why pass resolutions calling federal agents to attack our people and evict the camps as the drill digs beneath our sacred water? How powerful could we be if we agree to stand our ground on our treaty land where we have laid thousands of prayers?"
@@ -111,37 +117,28 @@ for entry in clean:
     print len(entry),entry
     input.append(word_tokenize(entry))
 
-""" STOP WORDS  """
-stopWords = set(stopwords.words('english'))
+""" STOP WORDS """
+with open(stopwords_file, 'r') as stop_file:
+    stopword_custom = stop_file.read().split("\n")
+stop_file.close()
+# concanete all the stopwords sources
+stop_words = stopwords.words('english') + list(string.punctuation) + stopword_custom + stopwords_list
 
-custom_stopfile = open(stopwords_file, 'r')
-custom_stop = custom_stopfile.read().split("\n")
-custom_stopfile.close()
-stopWords3 = ["'s", '...', '-800', 'mr.', 'de', 'een', '-0800', 'www.senate.gov']
-for punc in string.punctuation:
-    custom_stop.append(punc)
-for stp in stopWords3:
-    custom_stop.append(stp)
-
+""" LEMMATIZATION """
 lemmatizedList = list()
 lemmat = nltk.WordNetLemmatizer()
 for i in range(len(input)):
-    lemmatizedList.append(
-        [lemmat.lemmatize(x, j[0].lower()) if j[0].lower() in ['a', 'n', 'v'] else lemmat.lemmatize(x) for x, j in
-         pos_tag(input[i])])
+    lemmatizedList.append([lemmat.lemmatize(x, j[0].lower()) if j[0].lower() in ['a', 'n', 'v'] else lemmat.lemmatize(x) for x, j in pos_tag(input[i])])
 
 print len(lemmatizedList), lemmatizedList[0]
 
-pp = pprint.PrettyPrinter(indent=4)
-
 cleandata = list()
 for data in lemmatizedList:
-    cleandata.append([w for w in data if w.lower() not in stopwords.words('english') and w.lower() not in custom_stop])
+    cleandata.append([w for w in data if w.lower() not in stop_words])
 print len(cleandata), cleandata[2]
 
 """ GRAB WIKIPEDIA CONTENT """
-page_link = 'https://en.wikipedia.org/wiki/NODAPL' # or 'https://en.wikipedia.org/wiki/Dakota_Access_Pipeline'
-page_response = requests.get(page_link, timeout=5)
+page_response = requests.get(wikipage_link, timeout=5)
 # here, we fetch the content from the url, using the requests library
 page_content = BeautifulSoup(page_response.content, "html.parser")
 # we use the html parser to parse the url content and store it in a variable.
@@ -149,21 +146,19 @@ textContent = []
 for i in range(1, 7):
     paragraphs = page_content.find_all("p")[i].text
     textContent.append(paragraphs)
+
+pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(textContent)
 
-# FOR DAKOTA ACCESS PIPELINE
+# GET KEYWORDS / FREQUENT WORDS FOR NODAPL
 json_data = json.dumps(textContent)
 keywords = list()
 
-text_file = open("stopwords_new.txt", "r")
-stopWords2 = text_file.read().split('\n')
-text_file.close()
-# stopWords2 = set(stopwords.words('/home/maanav/Downloads/stopwords_en.txt'))
-stopWords3 = ["''", "``", "\\n", "'s", "'re", "the"]
+
 words = word_tokenize(json_data.lower())  # countin in lower
 wordsFiltered = []
 for w in words:
-    if w not in stopWords and w not in string.punctuation and w not in stopWords2 and w not in stopWords3:
+    if w not in stopwords:
         wordsFiltered.append(w)
 
 fd = nltk.FreqDist(wordsFiltered)
@@ -174,13 +169,12 @@ print(keywords)
 pp.pprint(most_freq)
 print(len(most_freq))
 
-"""# New Section"""
+
 keywords_synsets = list()
 
-for wordings in keywords:
-    temp_sets = wordnet.synsets(wordings)
-    temp_set_names = filter_synsets(temp_sets)
-    keywords_synsets.append(temp_set_names)
+for keyword in keywords:
+    keyword_synsets = get_synsets(keyword)
+    keywords_synsets.append(keyword_synsets)
 print(keywords_synsets)
 
 # use dummy data
@@ -199,8 +193,8 @@ print keywords[1], keywords_synsets[1], cleandata[1]
 wordFreq = numpy.zeros((len(cleandata), len(keywords)))
 for n in range(len(cleandata)):
     for i in range(len(keywords)):
-        # freq = count_frequency(keywords[i],keywords_synsets[i],cleandata[n])
-        freq = count_frequency_keyword_only(keywords, cleandata[n])
+        freq = count_frequency(keywords[i],keywords_synsets[i],cleandata[n])
+
         wordFreq[n][i] = freq
 
 print wordFreq
