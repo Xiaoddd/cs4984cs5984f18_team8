474a23464a17621105d28492692084f17358b580
Xiaoyu Chen <xiaoyuch@vt.edu>
2018-11-24 22:03:19 -0500

    Changed TF-IDF ranking strategy.

--- summary_initial.py
+++ summary_initial.py
@@ -1,23 +1,24 @@
 import json
 
-json_filename = 'small_relevant.json'
-json_file = open(json_filename)
-json_str = json_file.read()
-json_data = json.loads(json_str)
-#text = json_data[1]['Sentences']
-for i in range(0,int(len(json_data))):
-    text+= json_data[i]['Sentences']
-print(text)
-#for record in json_data:
-#    text += record['Sentences']
+# json_filename = 'preprocessed/big_relevant.json'
+# json_file = open(json_filename)
+# json_str = json_file.read()
+# json_data = json.loads(json_str)
+# #text = json_data[1]['Sentences']
+# text = ''
+# for i in range(0,int(len(json_data))):
+#     text+= json_data[i]['Sentences']
+# # print(text)
+# #for record in json_data:
+# #    text += record['Sentences']
 
-print(text)
+# # print(text)
 
 import os
 import re
-import nltk
-nltk.download('brown')
-from nltk.corpus import brown
+# import nltk
+# nltk.download('brown')
+# from nltk.corpus import brown
 import pickle
 import nltk
 import numpy as np
@@ -30,6 +31,13 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 # Noun Part of Speech Tags used by NLTK
 NOUNS = ['NN', 'NNS', 'NNP', 'NNPS']
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
 
 def clean_document(document):
     """Cleans document by removing unnecessary punctuation. It also removes
@@ -116,38 +124,59 @@ def rank_sentences(doc, doc_matrix, feature_names, top_n=25):
     return ranked_sents[:top_n]
 
 if __name__ == '__main__':
-    # Load corpus data used to train the TF-IDF Transformer
-    data = brown.words()
-    # Load the document you wish to summarize
-    title = 'American Missouri River Dakota Access Pipeline Fort Yates Standing Rock America Bakkan Sioux Youth Army Corps Engineer North Obama Trump Native DAPL Radio Energy Transfer Gonacon'
-    document = text
-    cleaned_document = clean_document(document)
-    doc = remove_stop_words(cleaned_document)
+    for t in range(5):
+        print('Start ranking topic '+str(t))
+        createFolder('preprocessed/Topics/Topic'+str(t)+'_Sents')
+        IDs_topic = np.loadtxt(open("preprocessed/Topics/Topic"+str(t)+".csv",'rb'),delimiter=",")
+        # Load corpus data used to train the TF-IDF Transformer
+        # data = brown.words()
+        sents = ''
+        NoDAPL_topic_file = 'preprocessed/Topics/Topic'+str(t)+'.json'
+        with open(NoDAPL_topic_file, 'r') as data_file:
+            raw = json.load(data_file)
+            for doc in raw:
+                sents += ' '
+                sents += doc['Sentences']
+            data = nltk.word_tokenize(sents)
+            print('-----finished corpus tokenization-----')
+            # Load the document you wish to summarize
+            title = 'American Missouri River Dakota Access Pipeline Fort Yates Standing Rock America Bakkan Sioux Youth Army Corps Engineer North Obama Trump Native DAPL Radio Energy Transfer Gonacon'
+            count = 0
+            for ele in raw:
+                document = ele['Sentences']
+                cleaned_document = clean_document(document)
+                doc = remove_stop_words(cleaned_document)
 
-    # Merge corpus data and new document data
-    data = [' '.join(document) for document in data]
-    train_data = set(data + [doc])
+                # Merge corpus data and new document data
+                data = [' '.join(document) for document in data]
+                train_data = set(data + [doc])
 
-    # Fit and Transform the term frequencies into a vector
-    count_vect = CountVectorizer()
-    count_vect = count_vect.fit(train_data)
-    freq_term_matrix = count_vect.transform(train_data)
-    feature_names = count_vect.get_feature_names()
+                # Fit and Transform the term frequencies into a vector
+                count_vect = CountVectorizer()
+                count_vect = count_vect.fit(train_data)
+                freq_term_matrix = count_vect.transform(train_data)
+                feature_names = count_vect.get_feature_names()
 
-    # Fit and Transform the TfidfTransformer
-    tfidf = TfidfTransformer(norm="l2")
-    tfidf.fit(freq_term_matrix)
+                # Fit and Transform the TfidfTransformer
+                tfidf = TfidfTransformer(norm="l2")
+                tfidf.fit(freq_term_matrix)
 
-    # Get the dense tf-idf matrix for the document
-    story_freq_term_matrix = count_vect.transform([doc])
-    story_tfidf_matrix = tfidf.transform(story_freq_term_matrix)
-    story_dense = story_tfidf_matrix.todense()
-    doc_matrix = story_dense.tolist()[0]
+                # Get the dense tf-idf matrix for the document
+                story_freq_term_matrix = count_vect.transform([doc])
+                story_tfidf_matrix = tfidf.transform(story_freq_term_matrix)
+                story_dense = story_tfidf_matrix.todense()
+                doc_matrix = story_dense.tolist()[0]
 
-    # Get Top Ranking Sentences and join them as a summary
-    top_sents = rank_sentences(doc, doc_matrix, feature_names)
-    summary = '.'.join([cleaned_document.split('.')[i]
-                        for i in [pair[0] for pair in top_sents]])
-    summary = ' '.join(summary.split())
-    print(summary)
+                # Get Top Ranking Sentences and join them as a summary
+                top_sents = rank_sentences(doc, doc_matrix, feature_names,top_n=1)
+                # print(top_sents)
+                summary = '.'.join([cleaned_document.split('.')[i]
+                                    for i in [pair[0] for pair in top_sents]])
+                summary = ' '.join(summary.split())
+                summary += '.'
+                outname = 'preprocessed/Topics/Topic'+str(t)+'_Sents/'+str(int(IDs_topic[count]))+'.txt'
+                with open(outname, "w") as text_file:
+                    text_file.write(summary)
+                count += 1
+                print("Completed {0}%".format(round(count/len(IDs_topic)*100)))
 
